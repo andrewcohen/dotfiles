af80219042a222678699ab288ee0438f7f63cfa0
Andrew Cohen <andrewcohen22@gmail.com>
2022-01-03 15:50:30 -0500

    cleanup

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -96,27 +96,6 @@ function M.setup()
       server:setup(opts)
   end)
 
-  --[[ local function setup_servers()
-    require'lspinstall'.setup()kV
-    local servers = require'lspinstall'.installed_servers()
-    for _, server in pairs(servers) do
-      nvim_lsp[server].setup{
-        on_attach = on_attach,
-        flags = {
-          debounce_text_changes = 150,
-        }
-      }
-    end
-  end
-
-  setup_servers() ]]
-
-  -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-  --[[ require'lspinstall'.post_install_hook = function ()
-    setup_servers() -- reload installed servers
-    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-  end ]]
-
   -- golang
   function goimports(timeout_ms)
     local context = { only = { "source.organizeImports" } }
