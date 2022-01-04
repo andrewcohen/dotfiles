e54b0b23f36918c397192c4cdca3d9584e62ca64
Andrew Cohen <andrewcohen22@gmail.com>
2022-01-03 21:42:41 -0500

    rust setup and prettier lsp ui

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -86,14 +86,26 @@ function M.setup()
       }
 
 
-      -- (optional) Customize the options passed to the server
-      -- if server.name == "tsserver" then
-      --     opts.root_dir = function() ... end
-      -- end
+      if server.name == "rust_analyzer" then
+        -- Initialize the LSP via rust-tools instead
+        require("rust-tools").setup {
+          -- The "server" property provided in rust-tools setup function are the
+          -- settings rust-tools will provide to lspconfig during init.            --
+          -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+          -- with the user's own settings (opts).
+          server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+        }
+        server:attach_buffers()
+      else
+        -- (optional) Customize the options passed to the server
+        -- if server.name == "tsserver" then
+        --     opts.root_dir = function() ... end
+        -- end
 
-      -- This setup() function is exactly the same as lspconfig's setup function.
-      -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-      server:setup(opts)
+        -- This setup() function is exactly the same as lspconfig's setup function.
+        -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+        server:setup(opts)
+      end
   end)
 
   -- golang
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -175,5 +175,18 @@ require('packer').startup(function(use)
   }
 
 
+  use {
+    'simrat39/rust-tools.nvim',
+    requires = {
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap'
+    },
+    config = function()
+      require('rust-tools').setup({})
+    end
+  }
+
+  use {'stevearc/dressing.nvim'}
 
 end)
