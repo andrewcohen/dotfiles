e9e150f2b35de9c96b9a069bbb5b3dd9aae34907
Andrew Cohen <andrewcohen22@gmail.com>
2024-11-13 15:19:43 -0500

    more updates

---
+++ .config/nvim/lua/config/lazy.lua
@@ -0,0 +1,42 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+vim.api.nvim_set_keymap('n', '<SPACE>', '<Nop>', {})
+
+vim.g.mapleader = " "
+
+vim.api.nvim_exec2([[
+autocmd BufLeave,FocusLost * silent! wall  " Save anytime we leave a buffer or MacVim loses focus.
+autocmd BufWritePre * :%s/\s\+$//e " strip trailing whitespace on save
+]], {})
+
+require('config.options')
+require('config.mappings')
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
